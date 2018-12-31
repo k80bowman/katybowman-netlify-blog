d68171ea0bc9ce2a4f7b7f5fd3eb77975ce2af81
Katy Bowman <katy@heysparkbox.com>
2018-12-30 22:03:59 -0500

    app cleanup and refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,40 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import ArticleCard from '../components/Article-Card';
 
-export default class IndexPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+const IndexPage = (props) => {
+  const { data } = props;
+  const { edges: posts } = data.allMarkdownRemark;
 
-    return (
-      <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-            </div>
-            {posts
-              .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #333', padding: '2em 4em' }}
-                  key={post.id}
-                >
-                  <p>
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <small>{post.frontmatter.date}</small>
-                  </p>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
-                    </Link>
-                  </p>
-                </div>
-              ))}
-          </div>
-        </section>
-      </Layout>
-    )
-  }
-}
+  return (
+    <Layout>
+      <section className="articles">
+        {posts
+          .map(({ node: post }) => (
+            <ArticleCard
+              slug={post.fields.slug}
+              key={post.id}
+              title={post.frontmatter.title}
+              date={post.frontmatter.date}
+              excerpt={post.frontmatter.excerpt}
+            />
+          ))}
+      </section>
+    </Layout>
+  );
+};
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
       edges: PropTypes.array,
     }),
-  }),
-}
+  }).isRequired,
+};
+
+export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexQuery {
@@ -76,4 +58,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
