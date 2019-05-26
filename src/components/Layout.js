f5385c8464a804f893fabe7fe2f4f6a2ad24a17b
Katy Bowman <katy@heysparkbox.com>
2019-05-25 21:43:32 -0400

    add favicons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -25,9 +25,13 @@ const TemplateWrapper = ({ children, location }) => (
           <title>{data.site.siteMetadata.title}</title>
           <meta name="description" content={data.site.siteMetadata.description} />
 
-          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
-          <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/img/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/favicon-16x16.png" />
+          <link rel="manifest" href="/img/favicons/site.webmanifest" />
+          <link rel="mask-icon" href="/img/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#2b5797" />
+          <meta name="theme-color" content="#ffffff" />
 
           <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
           <meta name="theme-color" content="#fff" />
