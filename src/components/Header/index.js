6cb00a0704c8f9f14f5e52d800872ba8f2f61cd4
Katy Bowman <katy@heysparkbox.com>
2020-05-19 20:46:29 -0400

    add reading link to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,11 +5,15 @@ import classnames from 'classnames';
 const Header = (location) => {
   const writerPage = location.location === 'writer';
   const devPage = location.location === 'developer';
+  const readingPage = location.location === 'reading';
   const writerLinkClass = classnames({
     current: writerPage,
   });
   const developerLinkClass = classnames({
     current: devPage,
+  });
+  const readingLinkClass = classnames({
+    current: readingPage,
   });
 
   return (
@@ -24,6 +28,7 @@ const Header = (location) => {
         <ul className="nav">
           <li><Link className={writerLinkClass} to="/writer/">writer</Link></li>
           <li><Link className={developerLinkClass} to="/developer/">developer</Link></li>
+          <li><Link className={readingLinkClass} to="/reading/">reading</Link></li>
         </ul>
       </div>
     </section>
