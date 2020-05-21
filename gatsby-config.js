f1efd45eef3bf396b1131170a748736acd3078bd
Katy Bowman <katy@heysparkbox.com>
2020-05-20 22:13:01 -0400

    add readings link and other cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,6 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'images',
-        path: `${__dirname}/src/img`,
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
         name: 'blog-posts',
         path: `${__dirname}/src/blog-posts`,
       },
--- src/components/Article-Card/index.js
+++ src/components/Article-Card/index.js
@@ -18,6 +18,7 @@ const ArticleCard = (props) => {
   const hasImage = imageLink && imageLink !== 'none';
   const imgClasses = classNames('article-card__excerpt-with-img', {
     'article-card--book-review': category === 'Book Review' || category === 'Reading',
+    'article-card--top-image': category !== 'Book Review' && category !== 'Reading',
   });
 
   let link;
